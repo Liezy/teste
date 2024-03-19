bbd99aa49b0e66f7ee60f6c6ad3e1a300c519aaa
Eliezer Alencar Moreira <alencareliezer20@gmail.com>
2024-03-18 21:09:59 -0300

    adiciona a feature deletar contatos

--- js/script.js
+++ js/script.js
@@ -18,6 +18,15 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+    tabelaContatosBody.addEventListener("click", function(event) {
+        if (event.target.classList.contains("btn-deletar")) {
+            const row = event.target.closest("tr");
+            tabelaContatosBody.deleteRow(row.rowIndex - 1);
+            // Atualiza os índices dos contatos após a exclusão
+            atualizarIndices();
+        }
+    });
+
     function adicionarContato(nome, numero) {
         const newRow = tabelaContatosBody.insertRow();
         const cellIndex = newRow.insertCell(0);
@@ -28,10 +37,28 @@ document.addEventListener("DOMContentLoaded", function() {
         cellIndex.textContent = contatoIndex;
         cellNome.textContent = nome;
         cellNumero.textContent = numero;
+        cellDelete.innerHTML = '<button class="btn btn-danger btn-deletar">Deletar</button>';
 
         contatoIndex++;
     }
+
+    function atualizarIndices() {
+        const linhas = tabelaContatosBody.rows;
+        for (let i = 0; i < linhas.length; i++) {
+            linhas[i].cells[0].textContent = i + 1;
+        }
+        contatoIndex = linhas.length + 1;
+    }
 });
+
+
+
+
+
+
+
+
+
 
 
 
